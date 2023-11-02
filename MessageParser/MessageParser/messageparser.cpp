da846276af67db8ee41e0ad6f96814e798e723e5
suriyakalaramanathan <suriyakala.ramanathan@gmail.com>
2023-11-02 00:01:53 -0400

    updated sample buffer

--- MessageParser/MessageParser/messageparser.cpp
+++ MessageParser/MessageParser/messageparser.cpp
@@ -5,54 +5,54 @@
 MESSAGEPARSER_API  MessageParser::MessageParser(const unsigned char* inputBuffer, int len) : buffer(inputBuffer), length(len) {}
 
 
-void MessageParser:: validateHeader() {
-    if (length < 10 || buffer[0] != 0x05 || buffer[1] != 0x08) {
-        throw exception("Invalid message format.");
-    }
+void MessageParser::validateHeader() {
+	if (length < 10 || buffer[0] != 0x05 || buffer[1] != 0x08) {
+		throw exception("Invalid message format.");
+	}
 }
 
 uint16_t MessageParser::extractBigEndianValue(int index) {
-    return (buffer[index] << 8) | buffer[index + 1];
+	return (buffer[index] << 8) | buffer[index + 1];
 }
 
-uint16_t MessageParser:: extractLittleEndianValue(int index) {
-    return (buffer[index] | (buffer[index + 1] << 8));
+uint16_t MessageParser::extractLittleEndianValue(int index) {
+	return (buffer[index] | (buffer[index + 1] << 8));
 }
 
 uint16_t MessageParser::calculateChecksum() {
-    uint16_t checksum = 0;
-    for (int i = 2; i <= 7; i++) {
-        checksum += buffer[i];
-    }
-    return checksum;
+	uint16_t checksum = 0;
+	for (int i = 2; i <= 7; i++) {
+		checksum += buffer[i];
+	}
+	return checksum;
 }
 
 MESSAGEPARSER_API  Node* MessageParser::parseMessage() {
-        try {
-            validateHeader();
+	try {
+		validateHeader();
 
-            uint16_t speed = extractBigEndianValue(2);
-            uint16_t heading = extractBigEndianValue(4);
-            uint16_t altitude = extractBigEndianValue(6);
-            uint16_t receivedChecksum = extractBigEndianValue(8);
+		uint16_t speed = extractBigEndianValue(2);
+		uint16_t heading = extractBigEndianValue(4);
+		uint16_t altitude = extractBigEndianValue(6);
+		uint16_t receivedChecksum = extractBigEndianValue(8);
 
-            uint16_t checksum = calculateChecksum();
+		uint16_t checksum = calculateChecksum();
 
-            // Verify against the given checksum in the message
-            if (checksum != receivedChecksum) {
-                throw exception("Checksum mismatch.");
-            }
+		// Verify against the given checksum in the message
+		if (checksum != receivedChecksum) {
+			throw exception("Checksum mismatch.");
+		}
 
-            // Create linked list with given values
-            Node* parsedValues = new Node(speed, " km/h");
-            parsedValues->next = new Node(heading, " degrees");
-            parsedValues->next->next = new Node(altitude, " meters");
+		// Create linked list with given values
+		Node* parsedValues = new Node(speed, "Speed(km/h)");
+		parsedValues->next = new Node(heading, "Heading(degrees)");
+		parsedValues->next->next = new Node(altitude, "Altitude(meters)");
 
-            return parsedValues;
-        }
-        catch (const std::exception& e) {
-            std::string errorMessage = "Parsing failed: ";
-            errorMessage += e.what();
-            throw std::runtime_error(errorMessage);
-        }
+		return parsedValues;
+	}
+	catch (const std::exception& e) {
+		std::string errorMessage = "Parsing failed: ";
+		errorMessage += e.what();
+		throw std::runtime_error(errorMessage);
+	}
 };
--- Parser/Parser/parser.cpp
+++ Parser/Parser/parser.cpp
@@ -8,64 +8,64 @@
 
 int main() {
 
-    try {
-        //uncomment these 3 lines and comment line 13 to line 27 to execute the success scenario with sample buffer.
+	try {
+		//uncomment these 3 lines and comment line 13 to line 27 to execute the success scenario with sample buffer.
 
-        //unsigned char inputMessage[] = { 0x05, 0x08, 0x01, 0xF4, 0x00, 0x64, 0x03, 0xE8, 0x44, 0x02 };
-       //int length = 10;
-        //MessageParser parser(inputMessage, length);
+		unsigned char inputMessage[] = { 0x05, 0x08, 0x01, 0xF4, 0x00, 0x64, 0x03, 0xE8, 0x02, 0x44 };
+		int length = 10;
+		MessageParser parser(inputMessage, length);
 
-        //////////////////////////////////////////
+		//////////////////////////////////////////
 
-        // Assuming the user will input the values
-      
-        std::vector<unsigned char> inputMessage;
-        int length;
+		// Assuming the user will input the values
 
-        std::cout << "Enter the length of the message: ";
-        std::cin >> length;
-   
+	  /*  std::vector<unsigned char> inputMessage;
+		int length;
 
-        for (int i = 0; i < length; ++i) {
-            unsigned int byte;
-            std::cin >> byte;
+		std::cout << "Enter the length of the message: ";
+		std::cin >> length;
 
-            std::stringstream ss;
-            ss << std::hex << "0x" << std::uppercase << std::setw(2) << std::setfill('0') << byte; // Ensuring two characters for each byte
 
-            std::string hexString = ss.str();
-            std::cout << "Hexadecimal representation: " << hexString << std::endl;
+		for (int i = 0; i < length; ++i) {
+			unsigned int byte;
+			std::cin >> byte;
 
-            inputMessage.push_back(static_cast<unsigned char>(byte));
-           
-        }
+			std::stringstream ss;
+			ss << std::hex << "0x" << std::uppercase << std::setw(2) << std::setfill('0') << byte; // Ensuring two characters for each byte
 
-        MessageParser parser(inputMessage.data(), length);
+			std::string hexString = ss.str();
+			std::cout << "Hexadecimal representation: " << hexString << std::endl;
 
-        ///////////////////////////////////////////////////////
-        Node* parsedValues = parser.parseMessage();
+			inputMessage.push_back(static_cast<unsigned char>(byte));
 
-        if (parsedValues != nullptr) {
-            std::cout << "Message Parsing successful. ";
-            Node* current = parsedValues;
-            while (current != nullptr) {
-                std::cout << "linked list values: " << current->value << current->valType << std::endl;
-                current = current->next;
-            }
+		}
 
-            // Free memory for the linked list
-            while (parsedValues != nullptr) {
-                Node* temp = parsedValues;
-                parsedValues = parsedValues->next;
-                delete temp;
-            }
-        }
-    }
-    catch (...)
-    {
-        std::cout << "Parsing Failed";
-    }
+		MessageParser parser(inputMessage.data(), length);
+		*/
+		///////////////////////////////////////////////////////
+		Node* parsedValues = parser.parseMessage();
 
-    return 0;
+		if (parsedValues != nullptr) {
+			std::cout << "Message Parsing successful. ";
+			Node* current = parsedValues;
+			while (current != nullptr) {
+				std::cout << "linked list values: " << current->value << current->valType << std::endl;
+				current = current->next;
+			}
+
+			// Free memory for the linked list
+			while (parsedValues != nullptr) {
+				Node* temp = parsedValues;
+				parsedValues = parsedValues->next;
+				delete temp;
+			}
+		}
+	}
+	catch (...)
+	{
+		std::cout << "Parsing Failed";
+	}
+
+	return 0;
 }
 
